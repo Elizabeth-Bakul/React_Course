d384297d38591b747e7df098f33f6f2232b8d2c4
Elizabeth-Bakul <bakelal@mail.ru>
2021-06-23 01:15:35 +0300

    nine task

--- src/Components/Functions/secondaryFunction.js
+++ src/Components/Functions/secondaryFunction.js
@@ -1,6 +1,14 @@
-export const totalPriceItems = order => order.price * order.count;
+export const totalPriceItems = order => {
+  const countTopping = order.topping && order.topping.filter(item => item.checked).length;
+  const priceTopping = (order.price * 0.1) * countTopping;
+  if (countTopping) {
+    return (order.price + priceTopping) * order.count;
+  } else {
+    return order.price * order.count;
+  }
+};
 
-export const formatCurrency = elem => elem.toLocaleString('ru-RU', {
+export const formatCurrency = value => value.toLocaleString('ru-RU', {
   style: 'currency',
   currency: 'RUB'
 });
